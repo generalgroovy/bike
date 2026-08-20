ad54ec5e038740e7538860a464603ffbefe54ace
generalgroovy <41192316+generalgroovy@users.noreply.github.com>
2026-08-20 19:38:24 +0200

    fix: load demand event retry module

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import './game-radio.js';
 import './game-riders.js';
 import './game-events.js';
+import './game-event-demand.js';
 import './game-progression.js';
 import './game-tools.js';
 export { Game } from './game-core.js';
